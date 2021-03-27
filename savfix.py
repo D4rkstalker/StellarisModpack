3cd432e2e0278d675cf315b0b1e7cabb9a0864b2
D4rkstalker <d4rkstalker127@gmail.com>
2021-03-27 18:11:32 +0800

    save test

--- savfix.py
+++ savfix.py
@@ -1,4 +1,4 @@
-savefile = open("gamestate.txt","r").readlines()
+savefile = open("test.txt","r").readlines()
 part1 = ""
 part2 = ""
 part3 = ""
@@ -8,18 +8,24 @@ level = 0
 for line in savefile:
     if not passed:
         part1 += (line + "\n")
-    else if passed:
+    elif passed:
         part3 += (line + "\n")
-    if = "last_created_pop=" in line:
+    if "last_created_pop=" in line:
         pop_ = False
         passed = True
     if pop_:
         part2 += (line + "\n")
     if "pop={" in line:
-        pop_ = True:
+        pop_ = True
 
 pops = re.findall(r'\w*? = {.*?\n}', part3, re.DOTALL)
 
 for pop in pops:
     if "species_index" not in pop:
-        part2 += pop
+        part2 += pop + "\n"
+savefile.close()
+with open("test.txt","w") as out:
+    out.write(part1)
+    out.write(part2)
+    out.write(part3)
+    
