1f134c4fe41b172d28596b3d924de63b6eb85eb9
D4rkstalker <d4rkstalker127@gmail.com>
2021-03-27 18:18:03 +0800

    Update savfix.py

--- savfix.py
+++ savfix.py
@@ -1,31 +1,14 @@
-savefile = open("test.txt","r").readlines()
-part1 = ""
-part2 = ""
-part3 = ""
-pop_ = False
-passed = False
-level = 0
-for line in savefile:
-    if not passed:
-        part1 += (line + "\n")
-    elif passed:
-        part3 += (line + "\n")
-    if "last_created_pop=" in line:
-        pop_ = False
-        passed = True
-    if pop_:
-        part2 += (line + "\n")
-    if "pop={" in line:
-        pop_ = True
+import re
 
-pops = re.findall(r'\w*? = {.*?\n}', part3, re.DOTALL)
+out = ""
+with open("test.txt","r") as in_file:
+    savefile = in_file.read()
 
-for pop in pops:
-    if "species_index" not in pop:
-        part2 += pop + "\n"
-savefile.close()
-with open("test.txt","w") as out:
-    out.write(part1)
-    out.write(part2)
-    out.write(part3)
-    
+    pops = re.findall(r'\w*? = {.*?\n}', savefile, re.DOTALL)
+
+    for pop in pops:
+        if "species_index" not in pop:
+            out += pop + "\n"
+
+with open("test.txt","w") as out_file:
+    out_file.write(out)
