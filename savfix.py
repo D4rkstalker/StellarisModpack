97e35f1244dbbdad362515626fdf83fd3ce5cb2c
D4rkstalker <d4rkstalker127@gmail.com>
2021-04-11 11:42:32 +0800

    reduced habitable worlds

--- savfix.py
+++ savfix.py
@@ -1,14 +1,18 @@
 import re
-
+species_index = 361
 out = ""
 with open("test.txt","r") as in_file:
     savefile = in_file.read()
-
-    pops = re.findall(r'\w*? = {.*?\n}', savefile, re.DOTALL)
-
+    #print(savefile)
+    #savefile = re.sub('\t*\n', '\n', savefile)
+    #savefile = re.sub(' *\n', '\n', savefile)
+    pops = re.findall(r'\t\d*?={.*?\n\t}', savefile, re.DOTALL)
+    print(len(pops))
     for pop in pops:
-        if "species_index" not in pop:
+        #print("\n\n*****\n\n\n")
+        #print(pop)
+        if "species_index" in pop:
             out += pop + "\n"
 
 with open("test.txt","w") as out_file:
-    out_file.write(out)
+   out_file.write(out)
